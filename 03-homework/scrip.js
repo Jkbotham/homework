cdcc3ea2198eafb33013ccf65b62cdde1dbfd481
James Botham <jamesbotham@gmail.com>
2019-10-05 01:42:26 -0500

    Small function changes and form edits

--- 03-homework/scrip.js
+++ 03-homework/scrip.js
@@ -4,45 +4,42 @@ var lCaseEl = document.getElementById("lowerCase");
 var uCaseEl = document.getElementById("upperCase");
 var genPw = document.getElementById("gen");
 var sent = document.getElementById("sent");
+var inputBox= document.getElementById("inputBox");
 
-
-    var i = document.getElementById("cRange"),
-        o = document.getElementById("sAmount");
+var i = document.getElementById("cRange"),
+    o = document.getElementById("sAmount");
 
     o.innerHTML = i.value;
 
-    i.addEventListener('input', function (){
-        o.innerHTML = i.value;
-    });
-    
+i.addEventListener('input', function (){
+    o.innerHTML = i.value;
+});
 
 genPw.addEventListener("click", function() {
     var j = i.value;
     var incChar = "";
     function passwordgen(length) {
-    var result = "";
-    var sChar = "!'(&)*+,-.%/:;<=>?@][$^_`{|#}~";
-    var uCase = "QWERTYUIOPASDFGHJKLZXCVBNM";
-    var lCase =  uCase.toLowerCase();
-    var bers = "1234567890";
-    var bothTogether = uCase+lCase;
-    var cLength = bothTogether.length;
-    var i = document.getElementById("cRange");
-    var array = [numberEl.checked, sCharEl.checked, lCaseEl.checked, uCaseEl.checked];
-    var trueArray = [bers, sChar, lCase, uCase];
+        var result = "";
+        var sChar = "!'(&)*+,-.%/:;<=>?@][$^_`{|#}~";
+        var uCase = "QWERTYUIOPASDFGHJKLZXCVBNM";
+        var lCase =  uCase.toLowerCase();
+        var bers = "1234567890";
+        var array = [numberEl.checked, sCharEl.checked, lCaseEl.checked, uCaseEl.checked];
+        var trueArray = [bers, sChar, lCase, uCase];
 
-    for ( var k = 0; k < array.length; k++ ) {
-        if (array[k] === true){
-              incChar += trueArray[k] ;
-            };;
-            };
+        for ( var k = 0; k < array.length; k++ ) {
+            if (array[k] === true){
+                incChar += trueArray[k] ;
+                };;
+                };
 
-    for ( var z = 0; z < length; z++ ) {
-        result += incChar.charAt(Math.floor(Math.random() * incChar.length));
-    }
-    return result;
-};
+        for ( var z = 0; z < length; z++ ) {
+            result += incChar.charAt(Math.floor(Math.random() * incChar.length));
+            }
+        return result;
+    };
     console.log(incChar, numberEl.checked,sCharEl.checked,lCaseEl.checked,uCaseEl.checked,j,passwordgen(j));
     sent.innerHTML = passwordgen(j);
+
 })
 
