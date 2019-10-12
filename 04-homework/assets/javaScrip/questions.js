9fe9a555e8040239a0781359f43d33fdfb0d23ce
James Botham <jamesbotham@gmail.com>
2019-10-12 01:36:43 -0500

    added more styling and corrected the nav bar.  Added all questions that will be used in the game to the questions js file.

---
+++ 04-homework/assets/javaScrip/questions.js
@@ -0,0 +1,99 @@
+var questions = [
+    {
+        title: "Whichis the youngest American City?",
+        choices: ["Killeen, TX", "Jacksonville, NC", "Paramount, CA", "Layton, CA"],
+        answer: "Jacksonville, NC"
+    },
+    {
+        title: "The Nile River is the longest river in the world (at 4,160 miles). Which one’s the next longest?",
+        choices: ["Yangtze River", "Congo River", "Amazon River", "Hunang He"],
+        answer: "Amazon River"
+    },
+    {
+        title: "What is the 10th most spoken language worldwide?",
+        choices: ["German", "Bengali", "Russian", "Portuguese"],
+        answer: "German"
+    },
+    {
+        title: "What do Grenada and Costa Rica have in common?",
+        choices: ["They have no army", "They sit on the Equator", "Voted world’s best place to live", "Countries with the least crime"],
+        answer: "They have no army"
+    },
+    {
+        title: "Vancouver has the SkyTrain, London has the London Underground. What’s the name of Hong Kong’s metro system?",
+        choices: ["Metrorail", "RTA Rapid Transit", "Docklands Light Railway", "MTR"],
+        answer: "MTR"
+    },
+    {
+        title: "Ouagadougou is the capital city of which African country?",
+        choices: ["Chad", "Burkina Faso", "Eritrea", "Djibouti"],
+        answer: "Burkina Faso"
+    },
+    {
+        title: "As of July 2011, Kosovo and the Vatican City are still not members of the United Nations. Who else isn’t?",
+        choices: ["Rwanda", "Taiwan", "Afghanistan", "El Salvador"],
+        answer: "Taiwan"
+    },
+    {
+        title: "Completed in 2012, The Shard (based in London) is Europe’s tallest building. How many floors does it have?",
+        choices: ["94", "68", "72", "81"],
+        answer: "72"
+    },
+    {
+        title: "The second longest coastline, after Canada, is where?",
+        choices: ["Chile", "Australia", "Russia", "Indonesia"],
+        answer: "Indonesia"
+    },
+    {
+        title: "Soekarno-Hatta, Guangzhou Baiyun and Madrid Barajas are all names for what?",
+        choices: ["Airports", "Bridges", "Race courses", "Coastal resorts"],
+        answer: "Airports"
+    },
+    {
+        title: " Which country actually has the world’s longest official name (except it’s known by a shortened version)?",
+        choices: ["Libya", "Guatemala", "Tajikistan", "Mongolia"],
+        answer: "Libya"
+    },
+    {
+        title: "Trabzon is a coastal city in north-eastern Turkey. Which sea does it border?",
+        choices: ["The Mediterranean Sea", "The Black Sea", "The Thracian Sea", "The Balearic Sea"],
+        answer: "The Black Sea"
+    },
+    {
+        title: "Approximately how many people live in Greenland (to the closest thousand)?",
+        choices: ["44,000", "32,000", "85,000", "57,000"],
+        answer: "57,000"
+    },
+    {
+        title: "In which country do you drive on the right side of the road?",
+        choices: ["Ireland", "Nepal", "Italy", "Malaysia"],
+        answer: "Italy"
+    },
+    {
+        title: "The Prime Meridian does not run through which continent?",
+        choices: ["Europe", "Africa", "Antarctica", "South America"],
+        answer: "South America"
+    },
+    {
+        title: "Which is the second largest city in New Zealand, after Auckland?",
+        choices: ["Christchurch", "Wellington", "Hamilton", "Napier-Hastings"],
+        answer: "Wellington"
+    },
+    {
+        title: "One of these countries isn’t landlocked. Which is it?",
+        choices: ["Zambia", "Paraguay", "Slovakia", "Croatia"],
+        answer: "Croatia"
+    },
+    {
+        title: "In 2012, which one of the following was not the name of a hurricane?",
+        choices: ["Patty", "Humberto", "Julian", "Valerie"],
+        answer: "Julian"
+    },
+    {
+        title: "Which country contains the most languages?",
+        choices: ["Papua New Guinea", "China", "Australia", "Jamaica"],
+        answer: "Papua New Guinea"
+    },
+];
+
+
