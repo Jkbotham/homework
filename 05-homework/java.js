a1372c93f7112fa1afe0e57e580597eb6f7a60ee
James Botham <jamesbotham@gmail.com>
2019-10-19 14:47:17 -0500

    finished writing java file and corrected a few errors. html needs rework yet

--- 05-homework/java.js
+++ 05-homework/java.js
@@ -1,13 +1,23 @@
 
 $(document).ready(function () {
-    
-    $(".rowt").empty();
+
+    var lengthOfDay = 9
+
+
+
+    // if (localStorage.getItem('dailyActivity')) {
+    //     dailyActivity = JSON.parse(localStorage.getItem('dailyActivity'))
+    //   } else {
+    //     dailyActivity = [];
+    //   }
+
+
+    // $(".rowt").empty();
     
     $("#currentDay").text(moment().format('dddd, MMMM Do'));
-    var t = 9;
 
-    for (i = 0; i < 9; i++) {
-        var hourOne = moment(t, "h");
+    for (i = 9; i < lengthOfDay + 9; i++) {
+        var hourOne = moment(i, "hh");
         var location = $(".rowt");
 
 
@@ -21,21 +31,23 @@ $(document).ready(function () {
         var textColm = $("<div>");
         $(textColm).attr("class", "col-md-8 txtColm");
         var textArea = $("<textarea>");
-        $(textArea).data("hour", hourOne.format("hha"));
+        $(textArea).attr("id", "hour" + i); 
+        $(textArea).attr("type", "text");
+        $(textArea).attr("value", "");         
 
         if (hourOne.isAfter(moment()) === true) {
             $(textArea).attr("class", "h-100 w-100 future");
-        } else if (hourOne.isBefore(moment()) === true) {
-            $(textArea).attr("class", "h-100 w-100 past");
-        } else {
+        } else if (hourOne.format('h') === moment().format('h')) {
             $(textArea).attr("class", "h-100 w-100 present");
+        } else {
+            $(textArea).attr("class", "h-100 w-100 past");
         };
 
         var divBtnCol = $("<div>");
         $(divBtnCol).attr("class", "col-md-2 btnColm");
         var btn = $("<button>");
         $(btn).attr("class", "btn btn-lg h-100 w-100 saveBtn");
-        $(btn).data("hour", hourOne.format("hha"));
+        $(btn).data("hour", "#" + hourOne.format("hha"));
         $(btn).attr("type", "button");
 
         var lock = $("<i>");
@@ -51,10 +63,37 @@ $(document).ready(function () {
         $(divRow).append(divBtnCol);
         $(location).append(divRow);
 
-        t++;
+
+
     }
 
+    for (i = 9; i < lengthOfDay + 9; i++){
+            
+        var input = "#hour" + i;
+    
+        var value = localStorage.getItem(input)
+    
+        $(input).text(value);
+    
+    }
     $(".saveBtn").on("click",function(){
-        console.log($(this).data("hour"))
+
+        var id = ($(this).data('hour'));
+
+        // console.log($(input).val());
+
+        for (i = 9; i < lengthOfDay + 9; i++){
+            
+            var input = "#hour" + i;
+
+            localStorage.setItem(input, $(input).val());
+
+            console.log($(input).val())
+
+
+        }
+
     })
+
 });
+
