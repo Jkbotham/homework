9a669e9a33e0fe14a83039dc5b11fcfe2d849a77
James Botham <jamesbotham@gmail.com>
2019-10-19 00:57:55 -0500

    Started basic scripting to homework 5.  Completed a loop to create every hour and to assign an attrib based upon the time of day

---
+++ 05-homework/java.js
@@ -0,0 +1,60 @@
+
+var t = 9;
+
+
+$(document).ready(function () {
+    
+    $(".rowt").empty();
+    
+    var t = 9;
+
+    for (i = 0; i < 9; i++) {
+        var hourOne = moment(t, "h");
+        var location = $(".rowt");
+
+
+        var divRow = $("<div>");
+        $(divRow).attr("class", "row time-block");
+
+        var hHour = $("<h4>");
+        $(hHour).attr("class", "hour")
+        $(hHour).text(hourOne.format('hha'));
+
+        var textColm = $("<div>");
+        $(textColm).attr("class", "col-md-8 txtColm");
+        var textArea = $("<textarea>");
+        $(textArea).attr("data", hourOne.format("hha"));
+
+        if (hourOne.isAfter(moment()) === true) {
+            $(textArea).attr("class", "h-100 w-100 future");
+        } else if (hourOne.isBefore(moment()) === true) {
+            $(textArea).attr("class", "h-100 w-100 past");
+        } else {
+            $(textArea).attr("class", "h-100 w-100 present");
+        };
+
+        var divBtnCol = $("<div>");
+        $(divBtnCol).attr("class", "col-md-2 btnColm");
+        var btn = $("<button>");
+        $(btn).attr("class", "btn btn-lg h-100 w-100 saveBtn");
+        $(btn).attr("data", hourOne.format("hha"));
+        $(btn).attr("type", "button");
+        var lock = $("<i>");
+        $(lock).attr("class", "fas fa-lock");
+        $(btn).append(lock);
+
+
+
+        $(divRow).append(hHour);
+        $(textColm).append(textArea);
+        $(divRow).append(textColm);
+        $(divBtnCol).append(btn);
+        $(divRow).append(divBtnCol);
+        $(location).append(divRow);
+
+        t++;
+        console.log(t);
+        console.log(hourOne.isAfter(moment()));
+        console.log(hourOne.isBefore(moment()));
+    }
+});
